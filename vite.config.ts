a346dadcf5a42fc7952d2b8ddc4e10da9e3df449
Eyobor616 <eyobor.elvis@gmail.com>
2026-02-23 10:04:01 +0100

    fixes on blank page on load

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { URL } from 'url';
+import pkg from './package.json';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/',
+      base: new URL(pkg.homepage).pathname,
       server: {
         port: 3000,
         host: '0.0.0.0',
